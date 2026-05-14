a4fd928cefca2c2a98b34215e4ca3e58547964fd
Hilary-Chan <hilary.hwh.c@gmail.com>
2026-05-15 08:49:07 +1000

    Update controller.py ?

--- src/motor/controller.py
+++ src/motor/controller.py
@@ -1,12 +1,23 @@
 from core.types import Component, MotorCommand
-# import Jetson.GPIO as GPIO
+
+import Jetson.GPIO as GPIO 
 
 class MotorController(Component):
   def __init__(self):
-    pass
+    GPIO.setmode(GPIO.BOARD) #gonna make us use board mode for now, lmk if no
+    
+    inputChannels = [] #put pin numbers here, but we don't know them yet so
+    outputChannels = []
+    
+    GPIO.setup(inputChannels, GPIO.IN)
+    GPIO.setup(outputChannels, GPIO.OUT)
 
+  
   def execute(self, cmd: MotorCommand) -> None:
+    #here i am taking the info from motorcommand and putting stuff into the pins
+    #turning_val and speed_val
+    #help
     pass
 
   def cleanup(self) -> None:
-    pass
+    GPIO.cleanup()
